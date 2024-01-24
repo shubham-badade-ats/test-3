1dddbabe1b7c8b409fe069a1f60856aa5633cc72
shubham-badade-ats <156400662+shubham-badade-ats@users.noreply.github.com>
2024-01-24 11:37:42 +0530

    chabges in jenkinsfile

--- script.py
+++ script.py
@@ -1,42 +1,3 @@
-# import docker
-
-# def remove_old_image(client, image_name):
-#     try:
-#         client.images.remove(image_name, force=True)
-#         print(f"Removed existing Docker image: {image_name}")
-#     except docker.errors.ImageNotFound:
-#         print(f"Docker image not found: {image_name}")
-
-# def pull_latest_image(client, image_name):
-#     print(f"Pulling the latest version of Docker image: {image_name}")
-#     client.images.pull(image_name)
-
-# def run_container(client, image_name):
-#     print(f"Running a container from the updated image: {image_name}")
-#     container = client.containers.run(image_name, detach=True)
-#     print(f"Container ID: {container.id}")
-
-# def main():
-#     # Set the name of the Docker image
-#     image_name = "docker-test"
-
-#     # Create a Docker client
-#     client = docker.from_env()
-
-#     # Remove the existing Docker image
-#     remove_old_image(client, shubhambadade07/test)
-
-#     # Pull the latest version of the Docker image
-#     pull_latest_image(client, shubhambadade07/test)
-
-#     # Run a container from the updated image
-#     run_container(client, shubhambadade07/test)
-
-# if __name__ == "__main__":
-#     main()
-
-
-
 import subprocess
 
 def remove_old_image(image_name):
@@ -52,21 +13,34 @@ def pull_latest_image(image_name):
 
 def run_container(image_name):
     print(f"Running a container from the updated image: {image_name}")
-    container_id = subprocess.run(["docker", "run", "-d", image_name], check=True, capture_output=True, text=True).stdout.strip()
+    container_id = subprocess.run(["docker", "run", "-d", "-p", "4200:4200", "--name", "test", image_name], check=True, capture_output=True, text=True).stdout.strip()
     print(f"Container ID: {container_id}")
 
+def stop_and_remove_container(container_name):
+    try:
+        subprocess.run(["docker", "stop", container_name], check=True)
+        print(f"Stopped Docker container: {container_name}")
+        subprocess.run(["docker", "rm", container_name], check=True)
+        print(f"Removed Docker container: {container_name}")
+    except subprocess.CalledProcessError:
+        print(f"Error stopping or removing Docker container: {container_name}")
+
 def main():
-    # Set the name of the Docker image
-    image_name = "docker-test"
+    # Set the name of the Docker image and container
+    image_name = "shubhambadade07/new-test"
+    container_name = "test"
 
     # Remove the existing Docker image
-    remove_old_image("shubhambadade07/new-test")
+    remove_old_image(image_name)
 
     # Pull the latest version of the Docker image
-    pull_latest_image("shubhambadade07/new-test")
+    pull_latest_image(image_name)
 
     # Run a container from the updated image
-    run_container("shubhambadade07/new-test")
+    run_container(image_name)
+
+    # Stop and remove the container by name
+    stop_and_remove_container(container_name)
 
 if __name__ == "__main__":
     main()
